697fd78af71d8117f7818e3a823b245d4dddf7d3
christiearcus <christie.arcus@gmail.com>
2016-03-30 16:02:39 +1100

    all functionality working except for winner function

--- naughts.js
+++ naughts.js
@@ -1,8 +1,8 @@
 // globals.
 
 var move = ""
-playerX = 0;
-playerO = 0;
+var playerX = 0;
+var playerO = 0;
 
 var gameGrid = [
   "", "", "",
@@ -10,62 +10,58 @@ var gameGrid = [
   "", "", ""
 ];
 
+document.getElementById('player-X').innerText = ' ' + playerX;
+document.getElementById('player-O').innerText = ' ' + playerO;
+
 // switch player;
 
-var whoGoesFirst = function() {
+var startRound = function() {
+  var addtoDiv = document.getElementById('starter');
   var firstUp = Math.floor(Math.random() * 2);
-
-  if (firstUp === 0) {
-    console.log('Crosses start this time');
+    if (firstUp === 0) {
+      addtoDiv.innerHTML = 'Player X begins this round';
     move = 'X';
-  }
-  else {
-    console.log("Naughts start this time");
+    }
+    else {
+      addtoDiv.innerHTML = 'Player O begins this round';
     move = 'O';
   }
 };
 
-whoGoesFirst();
+startRound();
 
 // interacting with grid
 
 var gameBoardUI = document.getElementById('game-board');
 gameBoardUI.addEventListener('click', function(event) {
-
   var index = Number(event.target.id);
-
-  if (move === 'X') {
-    event.target.innerText = move;
-    gameGrid[index] = move;
-    move = 'O';
-  }
-  else {
-    event.target.innerText = move;
-    gameGrid[index] = move;
-    move = 'X'
-  }
+    if (move === 'X') {
+      event.target.innerText = move;
+      gameGrid[index] = move;
+      move = 'O';
+      winner();
+    }
+    else {
+      event.target.innerText = move;
+      gameGrid[index] = move;
+      move = 'X';
+      winner();
+    }
 });
 
-// get a winner.
-
 var winner = function() {
-  if (gameGrid.indexOf("X") === 0) {
+  if (gameGrid.indexOf("X") === "[0,1,2]") {
     console.log('X is the winner of this game');
     playerX ++;
+    document.getElementById('player-X').innerText = playerX;
     clearBoard();
-    return gameGrid;
+    endGame();
+    startRound();
   }
-}
-
-var clearBoard = function() {
-  for (var i = 0; i < gameGrid.length; i ++) {
-    gameGrid[i] = "";
-    document.getElementById([i]).innerText = "";
-  }
-}
-  // ensure to add logic for edge case no winner.
-  // clear board for next game.
-  // announce next round.
+  else {
+    return console.log('no winner yet!')
+  };
+};
 
 // [2,4,6]
 // [0,4,8]
@@ -73,3 +69,26 @@ var clearBoard = function() {
 // [0,3,6]
 // [6,7,8]
 // [2,5,8]
+
+// clear board
+
+var clearBoard = function() {
+  for (var i = 0; i < gameGrid.length; i ++) {
+    gameGrid[i] = "";
+    document.getElementById([i]).innerText = "";
+  };
+};
+
+// end game
+
+var endGame = function() {
+  if (playerX === 3) {
+    alert('Player X is the ultimate champ!');
+  }
+  else if (playerO === 3) {
+    alert('Player O is the ultimate champ!');
+  }
+  else {
+    return;
+  };
+};
